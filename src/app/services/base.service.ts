2febaf7bbf0d7ec2bef0ff3ca2baf9679ded19c1
ruan silva <ruansilva@outlook.com>
2017-03-24 21:32:56 -0300

    change url server for https

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,4 +1,4 @@
 export class BaseService {
     // public static api = "http://192.168.25.3/RS.MeusPedidos.Services/api/";
-    public static api = "http://meuspedidos.azurewebsites.net/client/api/";
+    public static api = "https://meuspedidos.azurewebsites.net/client/api/";
 }
